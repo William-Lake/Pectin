580251a802fef6be126f61a46158929b6a7c0ada
William Lake <N/A>
2021-03-05 07:58:59 -0700

    May have gotten sidetracked...

--- create_db.sql
+++ create_db.sql
@@ -1,78 +1,68 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "object_type" (
-	"object_type_id"	INTEGER NOT NULL UNIQUE,
-	"name"	TEXT NOT NULL,
-	"description"	TEXT NOT NULL,
-	PRIMARY KEY("object_type_id" AUTOINCREMENT)
+CREATE TABLE IF NOT EXISTS `relationship_type` (
+	`relationship_type_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`object_type_id_1`	INTEGER NOT NULL,
+	`object_type_id_2`	INTEGER NOT NULL,
+	`name`	TEXT NOT NULL,
+	`description`	TEXT NOT NULL,
+	FOREIGN KEY(`object_type_id_2`) REFERENCES `object_type`(`object_type_id`),
+	FOREIGN KEY(`object_type_id_1`) REFERENCES `object_type`(`object_type_id`)
 );
-CREATE TABLE IF NOT EXISTS "attribute_type" (
-	"attribute_type_id"	INTEGER NOT NULL UNIQUE,
-	"name"	TEXT NOT NULL,
-	"description"	TEXT NOT NULL,
-	PRIMARY KEY("attribute_type_id" AUTOINCREMENT)
+CREATE TABLE IF NOT EXISTS `relationship` (
+	`relationship_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`relationship_type_id`	INTEGER NOT NULL,
+	`object_id_1`	INTEGER NOT NULL,
+	`object_id_2`	INTEGER NOT NULL,
+	`name`	TEXT NOT NULL,
+	FOREIGN KEY(`relationship_type_id`) REFERENCES `relationship_type`(`relationship_type_id`),
+	FOREIGN KEY(`object_id_1`) REFERENCES `object`(`object_id`),
+	FOREIGN KEY(`object_id_2`) REFERENCES `object`(`object_id`)
 );
-CREATE TABLE IF NOT EXISTS "relationship_type" (
-	"relationship_type_id"	INTEGER NOT NULL UNIQUE,
-	"object_type_id_1"	INTEGER NOT NULL,
-	"object_type_id_2"	INTEGER NOT NULL,
-	"name"	TEXT NOT NULL,
-	"description"	TEXT NOT NULL,
-	PRIMARY KEY("relationship_type_id" AUTOINCREMENT),
-	FOREIGN KEY("object_type_id_2") REFERENCES "object_type"("object_type_id"),
-	FOREIGN KEY("object_type_id_1") REFERENCES "object_type"("object_type_id")
+CREATE TABLE IF NOT EXISTS `question` (
+	`question_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`content_id`	INTEGER NOT NULL,
+	`target_table`	TEXT,
+	`target_id`	INTEGER,
+	`asked`	INTEGER NOT NULL DEFAULT 0 CHECK("asked" IN ( 0 , 1 )),
+	`created`	timestamp NOT NULL DEFAULT current_timestamp,
+	FOREIGN KEY(`content_id`) REFERENCES `content`(`content_id`)
 );
-CREATE TABLE IF NOT EXISTS "object" (
-	"object_id"	INTEGER NOT NULL UNIQUE,
-	"object_type_id"	INTEGER NOT NULL,
-	"name"	TEXT NOT NULL,
-	PRIMARY KEY("object_id" AUTOINCREMENT),
-	FOREIGN KEY("object_type_id") REFERENCES "object_type"("object_type_id")
+CREATE TABLE IF NOT EXISTS `object_type` (
+	`object_type_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`name`	TEXT NOT NULL,
+	`description`	TEXT NOT NULL
 );
-CREATE TABLE IF NOT EXISTS "attribute" (
-	"attribute_id"	INTEGER NOT NULL UNIQUE,
-	"attribute_type_id"	INTEGER NOT NULL,
-	"name"	TEXT NOT NULL,
-	PRIMARY KEY("attribute_id" AUTOINCREMENT),
-	FOREIGN KEY("attribute_type_id") REFERENCES "attribute_type"("attribute_type_id")
+CREATE TABLE IF NOT EXISTS `object` (
+	`object_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`object_type_id`	INTEGER NOT NULL,
+	`name`	TEXT NOT NULL,
+	FOREIGN KEY(`object_type_id`) REFERENCES `object_type`(`object_type_id`)
 );
-CREATE TABLE IF NOT EXISTS "relationship" (
-	"relationship_id"	INTEGER NOT NULL UNIQUE,
-	"relationship_type_id"	INTEGER NOT NULL,
-	"object_id_1"	INTEGER NOT NULL,
-	"object_id_2"	INTEGER NOT NULL,
-	"name"	TEXT NOT NULL,
-	PRIMARY KEY("relationship_id" AUTOINCREMENT),
-	FOREIGN KEY("relationship_type_id") REFERENCES "relationship_type"("relationship_type_id"),
-	FOREIGN KEY("object_id_2") REFERENCES "object"("object_id"),
-	FOREIGN KEY("object_id_1") REFERENCES "object"("object_id")
+CREATE TABLE IF NOT EXISTS `content` (
+	`content_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`text`	TEXT NOT NULL
 );
-CREATE TABLE IF NOT EXISTS "content" (
-	"content_id"	INTEGER NOT NULL UNIQUE,
-	"text"	TEXT NOT NULL,
-	PRIMARY KEY("content_id" AUTOINCREMENT)
+CREATE TABLE IF NOT EXISTS `attribute_type` (
+	`attribute_type_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`name`	TEXT NOT NULL,
+	`description`	TEXT NOT NULL
 );
-CREATE TABLE IF NOT EXISTS "answer" (
-	"answer_id"	INTEGER NOT NULL UNIQUE,
-	"question_id"	INTEGER NOT NULL,
-	PRIMARY KEY("answer_id" AUTOINCREMENT),
-	FOREIGN KEY("question_id") REFERENCES "question"("question_id")
+CREATE TABLE IF NOT EXISTS `attribute` (
+	`attribute_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`attribute_type_id`	INTEGER NOT NULL,
+	`name`	TEXT NOT NULL,
+	FOREIGN KEY(`attribute_type_id`) REFERENCES `attribute_type`(`attribute_type_id`)
 );
-CREATE TABLE IF NOT EXISTS "answer_content" (
-	"answer_content_id"	INTEGER NOT NULL UNIQUE,
-	"answer_id"	INTEGER NOT NULL,
-	"content_id"	INTEGER NOT NULL,
-	FOREIGN KEY("content_id") REFERENCES "content"("content_id"),
-	FOREIGN KEY("answer_id") REFERENCES "answer"("answer_id"),
-	PRIMARY KEY("answer_content_id" AUTOINCREMENT)
+CREATE TABLE IF NOT EXISTS `answer_content` (
+	`answer_content_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`answer_id`	INTEGER NOT NULL,
+	`content_id`	INTEGER NOT NULL,
+	FOREIGN KEY(`answer_id`) REFERENCES `answer`(`answer_id`),
+	FOREIGN KEY(`content_id`) REFERENCES `content`(`content_id`)
 );
-CREATE TABLE IF NOT EXISTS "question" (
-	"question_id"	INTEGER NOT NULL UNIQUE,
-	"content_id"	INTEGER NOT NULL,
-	"target_table"	TEXT NOT NULL,
-	"target_id"	INTEGER NOT NULL,
-	"asked"	INTEGER NOT NULL DEFAULT 0 CHECK("asked" IN (0, 1)),
-	"created"	timestamp NOT NULL DEFAULT current_timestamp,
-	FOREIGN KEY("content_id") REFERENCES "content"("content_id"),
-	PRIMARY KEY("question_id" AUTOINCREMENT)
+CREATE TABLE IF NOT EXISTS `answer` (
+	`answer_id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	`question_id`	INTEGER NOT NULL,
+	FOREIGN KEY(`question_id`) REFERENCES `question`(`question_id`)
 );
 COMMIT;
--- pectin/__main__.py
+++ pectin/__main__.py
@@ -1,6 +1,62 @@
 import logging
+from logging import StreamHandler,Formatter
+import re
 
 from db.models import *
+import sqlparse
+
+
+# Testing out the idea of formatting sql logs nicely.
+class SqlFormattingLogger(StreamHandler):
+
+    def __init__(self):
+
+        super().__init__()
+
+    def emit(self,record):
+
+        msg = record.msg[0]
+
+        args = record.msg[1]
+
+        if args:
+
+            # [:-1] takes off the closing paren
+            query_parts = msg.split('?')[:-1]
+
+
+            # Want this to look like a real sql statement,
+            # so going to detect strings (non-numbers essentially)
+            # and wrap them in single quotes.
+            formatted_args = []
+
+            for arg in args:
+
+                if isinstance(arg,str):
+
+                    formatted_args.append(f"'{arg}'")
+
+                else:
+
+                    formatted_args.append(str(arg))
+
+            # Create arg string
+            query_parts.append(', '.join(formatted_args))
+
+            # Replace trailing paren
+            query_parts.append(')')
+
+            msg = ''.join(query_parts)
+
+            args = []
+
+        # Formatting the sql to look nice,
+        # and putting space on either side of it.
+        msg = sqlparse.format(msg, keyword_case='upper', reindent=True,indent_columns=True)
+
+        record.msg = f'\n{msg}\n'
+
+        super().emit(record)
 
 
 def main(args):
@@ -29,10 +85,22 @@ def main(args):
     ])
     
     # Testing out creating a record and saving it. It works.
-    ot = ObjectType(description='desc',name='name')
+    # ot = ObjectType(description='desc',name='name')
     
-    ot.save()
-    
+    # ot.save()
+
+    # Unless the user is coming back to a previous session,
+    # there should always be some initial questions.
+    starting_question = 'At a high level, what does the application do?'
+
+    content = Content(text=starting_question)
+
+    content.save()
+
+    question = Question(content_id = content.content_id)
+
+    question.save()
+
 def gather_args():
     
     pass
@@ -43,8 +111,9 @@ if __name__ == '__main__':
     # by peewee are printed to the console.
     pw_logger = logging.getLogger('peewee')
     
-    pw_logger.addHandler(logging.StreamHandler())
-    
+    # Testing out the idea of formatting sql logs nicely.
+    pw_logger.addHandler(SqlFormattingLogger())
+
     pw_logger.setLevel(logging.DEBUG)
     
     main(gather_args())
